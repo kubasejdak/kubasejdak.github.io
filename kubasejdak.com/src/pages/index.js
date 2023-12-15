6161b571ae0bf6ce64ae6b3f7c58ad683b77ecdf
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2023-12-15 22:31:19 +0000

    Check 3.

--- kubasejdak.com/src/pages/index.js
+++ kubasejdak.com/src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations (2)
+        Congratulations (3)
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
