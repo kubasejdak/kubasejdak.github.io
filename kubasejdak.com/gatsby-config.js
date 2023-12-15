94ef78939ac9b0f72eab5f0a00e16e30edf9eeb3
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2023-12-15 22:06:05 +0000

    Do not install mdx support now.

--- kubasejdak.com/gatsby-config.js
+++ kubasejdak.com/gatsby-config.js
@@ -6,16 +6,5 @@ module.exports = {
     title: `kubasejdak.com`,
     siteUrl: `https://kubasejdak.com`,
   },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+  plugins: [],
 };
