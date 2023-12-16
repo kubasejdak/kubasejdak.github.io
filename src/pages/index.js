9136b268a35b7a9e6c8c8a0577de28e239207147
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2023-12-16 23:09:50 +0000

    Finalize initial deployment.

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations (6)
+        Congratulations (8)
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
