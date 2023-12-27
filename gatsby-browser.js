5c343a301db828a2e58ae3258bb80c9d02f0dc07
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2023-12-27 23:37:12 +0000

    Adjust code highlightling,

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
 require("prismjs/themes/prism-twilight.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-require("./styles/prismjs-fix-line-numbering.css");
+require("./styles/prismjs-adjust-highlighting.css");
